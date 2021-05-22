9ad684f950bcb0a71e8f6d2f66dbb95717f63e4c
raymondng76 <raymond.bcng@gmail.com>
2021-05-22 15:19:44 +0800

    add contains duplicates

---
+++ Code_Challenges/ContainsDuplicates.py
@@ -0,0 +1,16 @@
+# return true if any value appears at least twice in the array, else return false
+
+def containsDuplicate(nums):
+    sorted_lst = sorted(nums) # sorted first so same values are side by side
+    first_pt = 0
+    sec_pt = 1 # second pointer is one step in front
+    while sec_pt < len(nums):
+        if sorted_lst[first_pt] == sorted_lst[sec_pt]: # if side by side, same values will be true here
+            return True
+        first_pt += 1 # increase both pointers together
+        sec_pt += 1
+    return False
+
+
+nums = [1, 2, 3, 4]
+print(containsDuplicate(nums))
