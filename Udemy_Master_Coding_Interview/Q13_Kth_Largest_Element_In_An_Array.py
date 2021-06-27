924caae81a8ff33019c9919ae364d13aefd67b56
raymondng76 <raymond.bcng@gmail.com>
2021-06-27 20:52:15 +0800

    add horace_quick_select

--- Udemy_Master_Coding_Interview/Q13_Kth_Largest_Element_In_An_Array.py
+++ Udemy_Master_Coding_Interview/Q13_Kth_Largest_Element_In_An_Array.py
@@ -15,3 +15,28 @@ k2 = 4  # 4
 
 print(soln(nums1, k1))
 print(soln(nums2, k2))
+
+
+def soln2(nums: List[int], k: int) -> int:
+    def horace_quick_select(nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+        starter_idx = len(nums) - 1
+        swap_idx = 0
+        scan_idx = 0
+        while scan_idx < starter_idx:
+            if nums[scan_idx] > nums[starter_idx]:
+                scan_idx += 1
+            else:
+                nums[swap_idx], nums[scan_idx] = nums[scan_idx], nums[swap_idx]
+                swap_idx += 1
+                scan_idx += 1
+        nums[swap_idx], nums[starter_idx] = nums[starter_idx], nums[swap_idx]
+        left = horace_quick_select(nums[:swap_idx])
+        right = horace_quick_select(nums[swap_idx+1:])
+        return left + [nums[swap_idx]] + right
+    return horace_quick_select(nums)[-k]
+
+
+print(soln2(nums1, k1))
+print(soln2(nums2, k2))
